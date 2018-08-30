5136a9eeb69dc72628cfe892d9f0a3e24763095f
Nisson Nie <nskilly@gmail.com>
2018-08-30 00:38:38 -0400

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi, I am Nisson!</h1>
+    <p>Welcome to my new Gatsby site.</p>
+    <p>I am quite new to the coding world, I have done some stuff on my personal time and have learned from programming courses in university.</p>
+    <p>I hope to get learn more to be able to do many things I am not able to yet :)</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
